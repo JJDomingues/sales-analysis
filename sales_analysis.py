313c679cc4c7dca63a813ea86bff6b63f9725e7b
Josiberto Domingues <josibertotdomingues@gmail.com>
2026-04-22 09:51:27 -0300

    feat: add pandas dataframe and statistics analysis

--- sales_analysis.py
+++ sales_analysis.py
@@ -1,25 +1,30 @@
 import sqlite3
+import pandas as pd
 
-# Conectando ao banco de dados
 conn = sqlite3.connect("sales.db")
 cursor = conn.cursor()
 
-# Todos os clientes
+# SQL
 print("=== Todos os clientes: ===")
 cursor.execute("SELECT * FROM customers")
 for row in cursor.fetchall():
     print(row)
 
-# Cliente por cidade
 print("\n=== Clientes por cidade: ===")
 cursor.execute("SELECT city, COUNT(*) FROM customers GROUP BY city")
 for row in cursor.fetchall():
     print(row)
 
-# Média de idade
 print("\n=== Média de idade: ===")
 cursor.execute("SELECT AVG(age) FROM customers")
 print(cursor.fetchone()[0])
 
-# Fechando a conexão
+# Pandas
+print("\n=== DATAFRAME PANDAS ===")
+df = pd.read_sql_query("SELECT * FROM customers", conn)
+print(df)
+
+print("\n=== ESTATÍSTICAS ===")
+print(df.describe())
+
 conn.close()
